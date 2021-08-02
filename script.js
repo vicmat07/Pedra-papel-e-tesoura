3b9d3b8983a2d87767a2342523f9b712c30f993b
Victor Farias <victor.mateus.fa@hotmail.com>
2021-08-02 16:15:45 -0300

    Implementada a base da lógica do jogo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var el = document.querySelectorAll('.player-options div > img');
 var elEnemy=document.querySelectorAll('.enemy-options div > img');
-var Opt="", EnemyOpt="";
+var Opt="", EOpt="";
+var vencedor=document.getElementById('vencedor');
 
 function enemyOpt(){
     let randN=Math.floor(Math.random()*3);//gerar numero aletorio entre 0 e 2
@@ -8,6 +9,38 @@ function enemyOpt(){
     for(i=0;i<enemyOptions.length;i++){
         if(i==randN){
             enemyOptions[i].childNodes[0].style.opacity=1;
+            EOpt=document.querySelectorAll('.enemy-options div > img')[i].getAttribute('opt');
+        }
+    }
+}
+function validarVitoria(){    
+    vencedor.style.color='black';
+    if (Opt == "rock"){
+        if(EOpt == "rock"){
+            vencedor.innerHTML="Empate!";
+        } else if (EOpt == "paper") {
+            vencedor.innerHTML='pc ganhou';
+            vencedor.style.color='red';
+        } else if (EOpt == "scisor"){
+            vencedor.innerHTML='vc ganhou';
+        }
+    } else if (Opt == "paper") {
+        if(EOpt == "rock"){
+            vencedor.innerHTML='vc ganhou';
+        } else if (EOpt == "paper") {
+            vencedor.innerHTML='empate!';
+        } else if (EOpt == "scisor"){
+            vencedor.innerHTML='pc ganhou';
+            vencedor.style.color='red';
+        }
+    } else if (Opt == "scisor"){
+        if(EOpt == "rock"){
+            vencedor.innerHTML='pc ganhou';
+            vencedor.style.color='red';
+        } else if (EOpt == "paper") {
+            vencedor.innerHTML='vc ganhou!';
+        } else if (EOpt == "scisor"){
+            vencedor.innerHTML='empate';
         }
     }
 }
@@ -28,7 +61,8 @@ for(i=0;i<el.length;i++){
         t.target.style.opacity=1;
         Opt=t.target.getAttribute('opt');
         enemyOpt();
+        validarVitoria();
     })
 }
-let x=document.querySelectorAll('.enemy-options');
-console.log(x[0].children[0].children[0]);
+
+
