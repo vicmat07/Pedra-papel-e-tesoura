fac62866e5b439766488764e4826232abf190b55
Victor Farias <victor.mateus.fa@hotmail.com>
2021-08-02 14:15:45 -0300

    O player inimigo agora chuta pedra, papel ou tesoura quando o player seleciona uma opção entre as 3.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 var el = document.querySelectorAll('.player-options div > img');
-var Opt="";
+var elEnemy=document.querySelectorAll('.enemy-options div > img');
+var Opt="", EnemyOpt="";
 
+function enemyOpt(){
+    let randN=Math.floor(Math.random()*3);//gerar numero aletorio entre 0 e 2
+    const enemyOptions=document.querySelectorAll('.enemy-options div');
+    for(i=0;i<enemyOptions.length;i++){
+        if(i==randN){
+            enemyOptions[i].childNodes[0].style.opacity=1;
+        }
+    }
+}
 function resetarOpacidade(){
     for(i=0;i<el.length;i++){
         el[i].style.opacity=0.3;    
     }
 }
+function resetarOpacidadeEnemy(){
+    for(i=0;i<elEnemy.length;i++){
+        elEnemy[i].style.opacity=0.3;    
+    }
+}
 for(i=0;i<el.length;i++){
     el[i].addEventListener('click',(t)=>{
         resetarOpacidade();
+        resetarOpacidadeEnemy();
         t.target.style.opacity=1;
         Opt=t.target.getAttribute('opt');
-        alert(Opt);
+        enemyOpt();
     })
 }
+let x=document.querySelectorAll('.enemy-options');
+console.log(x[0].children[0].children[0]);
